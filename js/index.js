f02d8be731be91ac46b09d5409d1d1308cb6e3ca
sora <43514335+lits0ra@users.noreply.github.com>
2020-01-06 12:52:37 +0900

    HTMLとJSを分離 (#12)
    * 分離した
    
    * delete

---
+++ js/index.js
@@ -0,0 +1,45 @@
+count = 1
+let contents = [["レビュー内容", "バージョン", "レーティング"]]
+function tapped() {
+    if (document.getElementById("text-input").value == ''){
+        // text-inputが空っぽだった場合の処理
+        PNotify.notice('IDが入力されていません。');
+    } else {
+        try {
+            getReview(count)
+            count++
+            showPage()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+async function getReview(page) {
+    const id = document.getElementById("text-input").value;
+    const url = 'https://itunes.apple.com/jp/rss/customerreviews/page=' + page + '/id=' + id + '/json';
+    console.log(url)
+    const response = await fetch(url, { mode: 'cors' })
+    const json = await response.json()
+    json.feed.entry.forEach(function(value) {
+        contents.push([value.content.label, value['im:version'].label, value['im:rating'].label])
+        result.innerHTML += value.content.label + "<br/><br/>"
+    })
+    makeCSV()
+}
+
+function makeCSV() {
+    let csvData = "data:text/csv;charset=utf-8,"
+    contents.forEach(function(rows) {
+        const row = rows.join(",")
+        csvData += row + "\r\n"
+    })
+    const encodeUri = encodeURI(csvData)
+    const link = document.getElementById("download-csv")
+    link.setAttribute("href", encodeUri)
+    link.setAttribute("download", "csvdata.csv")
+}
+
+function showPage() {
+    const object = document.getElementById("output-page-number")
+    object.innerHTML = "next page = " + count
+}
