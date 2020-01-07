c99ee2a68d3e7aef07caa3e19294185f9904af02
sora <43514335+lits0ra@users.noreply.github.com>
2020-01-07 16:04:09 +0900

    countを消し去った (#16)
    * これを使うかどうかは不明ですね！
    
    * これは使うはずですね
    
    * エラーが出たときはcountをリセットするようにした
    
    * Fix ErrorMessage (#15)
    
    * とりあえずこうしておく
    
    * delete
    
    * スペルミス
    
    * delete
    
    * JSにexitはないみたいです
    
    * 削除
    
    * inputをぶちこんだ
    
    * いらない関数は削除
    
    * inputに変更することでユーザーが任意の数を入れることが可能になりました
    
    * エラーハンドリングはサボります
    
    * getReviewでIDが無効な場合valueを初期化する処理を追加&エラーメッセージを修正
    
    * countをdeleteした
    
    * 一度見たページは取得できないようにした
    
    Co-authored-by: ともき <tomoki_69386@yahoo.co.jp>

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-count = 1
+const showedPages = []
 let contents = [["レビュー内容", "バージョン", "レーティング"]]
 function tapped() {
-    if (document.getElementById("text-input").value == ''){
-        // text-inputが空っぽだった場合の処理
-        PNotify.notice('IDが入力されていません。');
-    } else {
-        try {
-            getReview(count)
-            count++
-            showPage()
-        } catch (error) {
-            console.log(error)
-        }
+    if (document.getElementById('text-input').value == '') {
+        PNotify.notice('IDが入力されていない、もしくはローマ字が含まれています。');
+        const objact = document.getElementById("output-page-number")
+        objact.value = 1
+        return
+    }
+    try {
+        const objact = document.getElementById("output-page-number")
+        getReview(objact.value)
+        objact.value = Number(objact.value) + 1
+    } catch (error) {
+        console.log(error)
+        PNotify.notice('不明なエラーが発生しました。')
     }
 }
 async function getReview(page) {
@@ -19,12 +21,29 @@ async function getReview(page) {
     const url = 'https://itunes.apple.com/jp/rss/customerreviews/page=' + page + '/id=' + id + '/json';
     console.log(url)
     const response = await fetch(url, { mode: 'cors' })
-    const json = await response.json()
-    json.feed.entry.forEach(function(value) {
-        contents.push([value.content.label, value['im:version'].label, value['im:rating'].label])
-        result.innerHTML += value.content.label + "<br/><br/>"
-    })
-    makeCSV()
+    const count = document.getElementById("output-page-number").value
+    try {
+        const json = await response.json()
+        const entry = json.feed.entry
+        if (entry == undefined) {
+            PNotify.notice('IDが無効です。');
+            const objact = document.getElementById("output-page-number")
+            objact.value = 1
+            return
+        } else if (showedPages.includes(count)) {
+            PNotify.notice('このページは一度取得したため追加することができません。')
+            return
+        }
+        entry.forEach(function(value) {
+            contents.push([value.content.label, value['im:version'].label, value['im:rating'].label])
+            result.innerHTML += value.content.label + "<br/><br/>"
+        })
+        showedPages.push(count)
+        makeCSV()
+    } catch (error) {
+        console.log(error)
+        PNotify.notice('なんらかのエラーが発生したため正常に取得できませんでした。')
+    }
 }
 
 function makeCSV() {
@@ -37,9 +56,4 @@ function makeCSV() {
     const link = document.getElementById("download-csv")
     link.setAttribute("href", encodeUri)
     link.setAttribute("download", "csvdata.csv")
-}
-
-function showPage() {
-    const object = document.getElementById("output-page-number")
-    object.innerHTML = "next page = " + count
 }
